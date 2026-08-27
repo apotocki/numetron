c3b5229de54bde97e776a09f4c6844537036bea2
apotocki <sane22222@aim.com>
2026-08-27 15:45:00 +0200

    fix: add three-way comparison operator for basic_integer_view with type mismatch handling

--- include/numetron/integer_view.hpp
+++ include/numetron/integer_view.hpp
@@ -415,6 +415,14 @@ requires(!std::is_same_v<LimbLT, LimbRT>)
     throw std::logic_error("basic_integer_view: comparison of different limb types is not supported yet");
 }
 
+template <std::unsigned_integral LimbLT, std::unsigned_integral LimbRT>
+requires(!std::is_same_v<LimbLT, LimbRT>)
+[[nodiscard]] inline std::strong_ordering operator <=>(basic_integer_view<LimbLT> lhs, basic_integer_view<LimbRT> rhs) noexcept(std::is_same_v<LimbLT, LimbRT>)
+{
+    (void)lhs; (void)rhs;
+    throw std::logic_error("basic_integer_view: comparison of different limb types is not supported yet");
+}
+
 template <std::unsigned_integral LimbT>
 basic_integer_view(std::span<LimbT>, int sign) -> basic_integer_view<LimbT>;
 
